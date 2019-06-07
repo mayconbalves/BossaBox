8c5ba88a5c8cb610b814e1f50043cd87e8914f24
mayconbalves <mayconmustaine@hotmail.com>
2019-06-06 22:03:25 -0300

    feat(root): add provider for all app

--- src/Root.js
+++ src/Root.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import Home from './containers/home/Home'
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path='/' component={Home} />
-    </Switch>
-  </Router>
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Router>
+  </Provider>
 )
+
+Root.prototypes = {
+  store: PropTypes.object.isRequired
+}
 
 export default Root
