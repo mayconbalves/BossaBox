e0b4a6669b4d1e4e8e6a401c41ce65f3a2aec283
mayconbalves <mayconmustaine@hotmail.com>
2019-06-06 22:04:19 -0300

    feat(index): import store and dispatch to router component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './Root'
+import store from '../src/store/store'
 
-ReactDOM.render(<Root />, document.getElementById('app'))
+ReactDOM.render(<Root store={store} />, document.getElementById('app'))
