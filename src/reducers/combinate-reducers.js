5f7a09cee5ebf1ceab585d94c485d3cd776a639c
mayconbalves <mayconmustaine@hotmail.com>
2019-06-11 21:33:07 -0300

    feat(*): add reducers for all app

--- src/reducers/combinate-reducers.js
+++ src/reducers/combinate-reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import homeReducer from './homeReducer'
 
 
 const rootReducer = combineReducers({
   // all reducers
+  homeReducer
 })
 
 export default rootReducer
