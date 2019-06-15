1f5fedd218e3a4d6bd3542ef2ebd69090fbeb8e0
mayconbalves <mayconmustaine@hotmail.com>
2019-06-15 12:34:37 -0300

    feat(*): create comun styled with theme

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    color: #170C3A;
+    font-family: Source Sans Pro, Regular;
+    font-size: 20px;
+  }
+
+  h1 {
+    color: #170C3A;
+    font-family: Source Sans Pro, Semibold;
+    font-size: 42px;
+  }
+
+  h2 {
+    color: #170C3A;
+    font-family: Source Sans Pro, Semibold;
+    font-size: 36px;
+  }
+`
+
