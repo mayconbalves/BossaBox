a71969a00b8f863844d69728218d7438192d7390
mayconbalves <mayconmustaine@hotmail.com>
2019-06-15 12:35:38 -0300

    feat(*): import globaly styled

--- src/Root.js
+++ src/Root.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import Home from './containers/home/Home'
+import { GlobalStyle } from './theme'
 
 const Root = ({ store }) => (
   <Provider store={store}>
+    <GlobalStyle />
     <Router>
       <Switch>
         <Route exact path='/' component={Home} />
