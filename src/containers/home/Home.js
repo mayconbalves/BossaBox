c3bc28a5236921c199764eb726bcb3bfc785b83b
mayconbalves <mayconmustaine@hotmail.com>
2019-06-17 21:46:33 -0300

    feat(home): create first row with inputs and button add

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -3,8 +3,15 @@ import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import * as toolsActions from './actions'
+import styled from 'styled-components'
 
 import Grid from '../../components/grid/Grid'
+import Row from '../../components/row/Row'
+import Col from '../../components/col/Col'
+
+const StyledButton = styled.button`
+  float: right;
+`
 
 class HomeContainer extends Component {
 
@@ -14,11 +21,21 @@ class HomeContainer extends Component {
     fetchTools()
   }
   render() {
-    console.log(this.props)
     return (
       <Grid>
         <h1>VUTTR</h1>
         <p>Very Useful Tools to Remember</p>
+        <Row>
+          <Col xs={3} md={3} lg={3}>
+            <input />
+          </Col>
+          <Col xs={3} md={3} lg={3}>
+            <input />
+          </Col>
+          <Col xs={6} md={6} lg={6}>
+            <StyledButton>+ Add</StyledButton>
+          </Col>
+        </Row>
       </Grid>
     )
   }
