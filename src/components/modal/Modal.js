cdcf44f3e5beafa6f86b85249fcff425f04bfaf3
mayconbalves <mayconmustaine@hotmail.com>
2019-06-18 21:55:55 -0300

    feat(modal): create component

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Modal extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div>
+        Modal
+      </div>
+    )
+  }
+}
+
+export default Modal
