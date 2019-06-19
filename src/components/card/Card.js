7a4e4d02379dc9b8e9ee3f4d9c0cbaf3797d354e
mayconbalves <mayconmustaine@hotmail.com>
2019-06-18 22:07:45 -0300

    chore(card): create style for card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import Row from '../row/Row'
 import Col from '../col/Col'
 
+const CardBody = styled.div`
+  width: 100%;
+  height: 100px;
+  border: 2px solid black;
+  margin: 15px auto;
+`
+
 const Card = ({ description, handleDeleteTool, id, link, tags, title }) => {
   const hashTags = tags || []
   return (
-    <div>
+    <CardBody>
       <Row>
         <Col>
           {title}
@@ -32,7 +40,7 @@ const Card = ({ description, handleDeleteTool, id, link, tags, title }) => {
           ))
         }
       </Row>
-    </div>
+    </CardBody>
   )
 }
 
