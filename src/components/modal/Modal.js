8b80a03794a0b193e96d2e2d905945a4e1b5e12f
mayconbalves <mayconmustaine@hotmail.com>
2019-06-19 22:05:57 -0300

    feat(modal): create submit function

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const ModalBody = styled.div`
+  background: #fff;
+  border: 1px solid red;
+`
 
 class Modal extends Component {
   state = {}
 
+  handleSubmit = () => {
+    const { fetchAddTools } = this.props
+
+    fetchAddTools()
+  }
   render() {
     return (
-      <div>
-        Modal
-      </div>
+      <ModalBody>
+        <h1>Modal</h1>
+        <div>
+          <button onClick={this.handleSubmit}>Add Tools</button>
+        </div>
+      </ModalBody>
     )
   }
+}
+
+Modal.propTypes = {
+  fetchAddTools: PropTypes.func.isRequired
 }
 
 export default Modal
