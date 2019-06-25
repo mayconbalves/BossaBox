1d9d7c8d9542f76d21a6a6104f99f320c8111752
mayconbalves <mayconmustaine@hotmail.com>
2019-06-24 22:47:06 -0300

    feat(reducer): create case of delete and add tools

--- src/reducers/homeReducer.js
+++ src/reducers/homeReducer.js
@@ -1,6 +1,10 @@
 import {
   GET_TOOLS_ERROR,
   GET_TOOLS_SUCCESS,
+  DELETE_TOOLS_ERROR,
+  DELETE_TOOLS_SUCCESS,
+  ADD_TOOLS_ERROR,
+  ADD_TOOLS_SUCCESS
 } from '../containers/home/types'
 
 const initialState = {
@@ -17,6 +21,28 @@ export default function character (state = initialState, action) {
       error: null
     }
   case GET_TOOLS_ERROR:
+    return {
+      tools: [],
+      error: action.payload
+    }
+  case DELETE_TOOLS_SUCCESS:
+    return {
+      ...state,
+      tools: [action.payload],
+      error: null
+    }
+  case DELETE_TOOLS_ERROR:
+    return {
+      tools: [],
+      error: action.payload
+    }
+  case ADD_TOOLS_SUCCESS:
+    return {
+      ...state,
+      tools: [action.payload],
+      error: null
+    }
+  case ADD_TOOLS_ERROR:
     return {
       tools: [],
       error: action.payload
