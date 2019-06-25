e8c8550a9d222888868502f6cf349d779fe65bb7
mayconbalves <mayconmustaine@hotmail.com>
2019-06-24 22:47:48 -0300

    feat(modal): create fake inputs for add tools

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -8,18 +8,64 @@ const ModalBody = styled.div`
 `
 
 class Modal extends Component {
-  state = {}
+  state = { title: '', link: '', description: '', tags: [] }
+
+  handleChange = e => {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+
+    this.setState({ [name]: value })
+  }
 
   handleSubmit = () => {
     const { fetchAddTools } = this.props
+    const { title, link, description, tags } = this.state
 
-    fetchAddTools()
+    const tagsArray = []
+    tagsArray.push(tags)
+
+    const params = {
+      title,
+      link,
+      description,
+      tagsArray
+    }
+
+    fetchAddTools(params)
   }
   render() {
+    const { title, link, description, tags } = this.state
     return (
       <ModalBody>
         <h1>Modal</h1>
         <div>
+          <input
+            type='text'
+            name='title'
+            value={title}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type='text'
+            name='link'
+            value={link}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name='description'
+            value={description}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name='tags'
+            value={tags}
+            onChange={this.handleChange}
+            required
+          />
           <button onClick={this.handleSubmit}>Add Tools</button>
         </div>
       </ModalBody>
