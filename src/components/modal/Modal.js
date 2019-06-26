9e22b2b4b3b374518bd93a5ab21c63d31745fabf
mayconbalves <mayconmustaine@hotmail.com>
2019-06-25 22:15:13 -0300

    chore(modal): create initial style of fields

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+import Row from 'components/row/Row'
+import Col from 'components/col/Col'
+
 const ModalBody = styled.div`
   background: #fff;
   border: 1px solid red;
+`
+
+const ModalContent = styled.div`
+  margin: 10px;
+  border: 1px solid #000;
+`
+
+const InputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+
+  label {
+    position: absolute;
+  }
+
+  input, textarea {
+    width: 90%;
+    margin: 20px;
+    padding: 10px;
+  }
 `
 
 class Modal extends Component {
@@ -38,36 +61,66 @@ class Modal extends Component {
     const { title, link, description, tags } = this.state
     return (
       <ModalBody>
-        <h1>Modal</h1>
-        <div>
-          <input
-            type='text'
-            name='title'
-            value={title}
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            type='text'
-            name='link'
-            value={link}
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            name='description'
-            value={description}
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            name='tags'
-            value={tags}
-            onChange={this.handleChange}
-            required
-          />
+        <ModalContent>
+        <h4>+ Add new tool</h4>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+              <InputWrapper>
+                <label>Tool Name</label>
+                <input
+                  type='text'
+                  name='title'
+                  value={title}
+                  onChange={this.handleChange}
+                  required
+                />
+              </InputWrapper>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+              <InputWrapper>
+                <label>Tool Link</label>
+                <input
+                  type='text'
+                  name='link'
+                  value={link}
+                  onChange={this.handleChange}
+                  required
+                />
+              </InputWrapper>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+              <InputWrapper>
+                <label>Tool Description</label>
+                <textarea
+                  rows='4'
+                  cols='30'
+                  name='description'
+                  value={description}
+                  onChange={this.handleChange}
+                  required
+                />
+               </InputWrapper>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+              <InputWrapper>
+                <label>Tags</label>
+                <input
+                  name='tags'
+                  value={tags}
+                  onChange={this.handleChange}
+                  required
+                />
+              </InputWrapper>
+            </Col>
+          </Row>
           <button onClick={this.handleSubmit}>Add Tools</button>
-        </div>
+        </ModalContent>
       </ModalBody>
     )
   }
