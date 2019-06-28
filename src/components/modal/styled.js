da152c559612e84514c9b61d03dde1a3c0dad060
mayconbalves <mayconmustaine@hotmail.com>
2019-06-27 23:01:00 -0300

    chore(modal): create final styled modal

--- src/components/modal/styled.js
+++ src/components/modal/styled.js
@@ -1,13 +1,31 @@
 import styled from 'styled-components'
 
 export const ModalBody = styled.div`
-  background: #fff;
-  border: 1px solid red;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0,0.4);
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  overflow: auto;
+  padding-top: 80px;
+  display: block;
+
+  h5 {
+    margin-left: 15px;
+  }
 `
 
 export const ModalContent = styled.div`
-  margin: 10px;
-  border: 1px solid #000;
+  background-color: white;
+  width: 70%;
+  max-width: 640px;
+  height: 75%;
+  margin: auto;
+  padding: 10px;
+  border: 3px solid black;
 `
 
 export const InputWrapper = styled.div`
@@ -24,5 +42,15 @@ export const InputWrapper = styled.div`
     width: 90%;
     margin: 20px;
     padding: 10px;
+    border: 2px solid;
   }
+`
+
+export const StyledButton = styled.button`
+  float: right;
+  color: #fff;
+  margin-right: 15px;
+  width: 174px;
+  height: 50px;
+  background: #365DF0;
 `
