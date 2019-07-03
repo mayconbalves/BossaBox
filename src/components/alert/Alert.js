4abef64b95555cb79fa8e15c3d1703b33c554899
mayconbalves <mayconmustaine@hotmail.com>
2019-07-02 23:31:46 -0300

    chore(alert): styled component

--- src/components/alert/Alert.js
+++ src/components/alert/Alert.js
@@ -1,15 +1,53 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const AlertBody = styled.div`
+  background-color: rgba(0,0,0,0.4);
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  overflow: auto;
+  padding-top: 80px;
+  display: block;
+
+  h5 {
+    margin-left: 15px;
+  }
+`
+
+const AlertContent = styled.div`
+  background-color: white;
+  width: 70%;
+  max-width: 640px;
+  height: 30%;
+  margin: auto;
+  padding: 10px;
+  border: 3px solid black;
+`
+
+const StyledButton = styled.button`
+  color: #fff;
+  margin-right: 15px;
+  width: 174px;
+  height: 50px;
+  background: #365DF0;
+`
 
 const Alert = (props) => {
   const { handleDeleteTool, setAlert, id, tool } = props
   return (
-    <div>
-      <h4>x Remove tool</h4>
-    <p>Are you sure wnat to remove {tool}</p>
-    <button onClick={() => setAlert(false)}>Cancel</button>
-    <button onClick={() => handleDeleteTool(id)}>Yes, remove</button>
-    </div>
+    <AlertBody>
+      <AlertContent>
+        <h4>x Remove tool</h4>
+        <p>Are you sure wnat to remove {tool} ?</p>
+        <StyledButton onClick={() => setAlert(false)}>Cancel</StyledButton>
+        <StyledButton onClick={() => handleDeleteTool(id)}>Yes, remove</StyledButton>
+      </AlertContent>
+    </AlertBody>
   )
 }
 
